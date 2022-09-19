feb2cb1d82c3fc66b5ec83fe730700dedbcd1b80
Asif Mahmud <asifmahmud3472@gmail.com>
2022-09-19 15:01:14 -0400

    exercise-1

---
+++ Exercises/Exercise1/Exercise1/Data/PetShopInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Exercise1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Exercise1.Data
+{
+    public class PetShopInitializer : DropCreateDatabaseAlways<PetShopContext>
+    {
+        protected override void Seed(PetShopContext context)
+        {
+            Animal dog = new Animal { Name = "Dog"};
+            Animal cat = new Animal { Name = "Cat"};
+
+            context.Animals.Add(dog);
+            context.Animals.Add(cat);
+
+            base.Seed(context);
+        }
+    }
+}
