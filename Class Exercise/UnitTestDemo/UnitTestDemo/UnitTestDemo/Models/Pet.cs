a486befee4918e04544d6e72b2252d30e2ab30df
Asif Mahmud <c0837117@mylambton.ca>
2022-09-22 16:18:52 -0400

    adding unit test demo

---
+++ Class Exercise/UnitTestDemo/UnitTestDemo/UnitTestDemo/Models/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnitTestDemo.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool isMale { get; set; }
+
+        public bool ifFixed { get; set; }
+
+        public ApplicationUser owner { get; set; }
+    }
+}
